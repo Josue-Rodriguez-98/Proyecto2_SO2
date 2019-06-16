7ebde4a11abeaa0b04b4916b079f695b011c449e
Josue David <josue.rodriguez1551@gmail,com>
2019-06-15 21:52:26 -0600

    Conexion entre servidor y cliente; muestra arbol de archivos en ambos

---
+++ Proyecto2_SO2/src/Com.java
@@ -0,0 +1,85 @@
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Iterator;
+import java.util.Vector;
+import javax.swing.tree.DefaultTreeModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Josue Rodriguez
+ */
+public class Com extends UnicastRemoteObject implements ComInterface{
+
+    public String name;
+    public DefaultTreeModel model;
+    public Vector<ComInterface> clients = new Vector();
+    
+    public Com(String name) throws RemoteException{
+        this.name = name;
+    }
+    
+    public Com(String name, DefaultTreeModel model) throws RemoteException{
+        this.name = name;
+        this.model = model;
+    }
+    
+    @Override
+    public String getName() throws RemoteException {
+        return this.name;
+    }
+
+    @Override
+    public DefaultTreeModel getModel() throws RemoteException {
+        return this.model;
+    }
+
+    @Override
+    public void sendTree() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setModel(DefaultTreeModel modelo) throws RemoteException {
+        this.model = modelo;
+    }
+
+    @Override
+    public void setClient(ComInterface cliente) throws RemoteException {
+        clients.add(cliente);
+    }
+
+    @Override
+    public void send(String msg) throws RemoteException {
+        if(msg.equals("nuevoCliente")){
+            Iterator<ComInterface> it = this.clients.iterator();
+            while(it.hasNext()){
+                ComInterface actual = it.next();
+                it.next().setModel(this.model);
+                actual.printMessage("Cambios en el arbol, presione refrescar!");
+            }
+        }
+    }
+
+    @Override
+    public void printMessage(String s) throws RemoteException {
+        System.out.println(s);
+    }
+
+    @Override
+    public void nuevoCliente() throws RemoteException {
+        Iterator<ComInterface> it = this.clients.iterator();
+            while(it.hasNext()){
+                ComInterface actual = it.next();
+                it.next().setModel(this.model);
+                actual.printMessage("Cambios en el arbol, presione refrescar!");
+            }
+    }
+    
+}
---
+++ Proyecto2_SO2/src/ComInterface.java
@@ -0,0 +1,26 @@
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import javax.swing.tree.DefaultTreeModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Josue Rodriguez
+ */
+public interface ComInterface extends Remote{
+    public String getName() throws RemoteException;
+    public DefaultTreeModel getModel() throws RemoteException;
+    public void setModel(DefaultTreeModel modelo) throws RemoteException;
+    public void sendTree() throws RemoteException;
+    public void send(String msg) throws RemoteException;
+    public void setClient(ComInterface cliente) throws RemoteException;
+    public void printMessage(String s) throws RemoteException;
+    public void nuevoCliente() throws RemoteException;
+    
+}
